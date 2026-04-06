c1014bb37cb2eb9339ec71d7119d7059a1861759
LasponaSilverioM <tawinlaspona@gmail.com>
2026-04-06 15:58:17 +0800

    fix changes

---
+++ src/pages/Menu.tsx
@@ -0,0 +1,65 @@
+import {
+  IonButton,
+  IonButtons,
+  IonIcon,
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonMenu,
+  IonMenuButton,
+  IonMenuToggle,
+  IonPage,
+  IonRouterOutlet,
+  IonSplitPane,
+  IonTitle,
+  IonToolbar
+} from "@ionic/react";
+import { Redirect, Route } from "react-router";
+import { homeOutline, logOutOutline } from "ionicons/icons";
+import Home from "./Home";
+
+const Menu: React.FC = () => {
+  const path = [
+    { name: 'Home', url: '/app/home', icon: homeOutline }
+  ];
+
+  return (
+    <IonPage>
+      <IonSplitPane contentId="main">
+        <IonMenu contentId="main">
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>Menu</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+
+          <IonContent>
+            {path.map((item, index) => (
+              <IonMenuToggle key={index}>
+                <IonItem routerLink={item.url} routerDirection="forward">
+                  <IonIcon icon={item.icon} slot="start" />
+                  {item.name}
+                </IonItem>
+              </IonMenuToggle>
+            ))}
+
+            <IonButton routerLink="/" routerDirection="back" expand="full">
+              <IonIcon icon={logOutOutline} slot="start" />
+              Logout
+            </IonButton>
+          </IonContent>
+        </IonMenu>
+
+        <IonRouterOutlet id="main">
+          <Route exact path="/app/home" component={Home} />
+          <Route exact path="/app">
+            <Redirect to="/app/home" />
+          </Route>
+        </IonRouterOutlet>
+      </IonSplitPane>
+    </IonPage>
+  );
+};
+
+export default Menu;
+
