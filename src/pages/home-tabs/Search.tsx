c7542cbc2d1a74a75ab16982688b879b56635c3a
LasponaSilverioM <tawinlaspona@gmail.com>
2026-04-23 09:08:16 +0800

    added search

--- src/pages/home-tabs/Search.tsx
+++ src/pages/home-tabs/Search.tsx
@@ -1,21 +1,46 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import { IonButtons, IonContent, IonHeader, IonItem, IonMenuButton, IonPage, IonSearchbar, IonTitle, IonToolbar } from "@ionic/react";
+import {  useState} from 'react';
 
 const Search: React.FC = () => {
+
+const data =  [
+
+'ariel',
+'achas',
+'diether',
+'banban',
+'yanyan',
+'aldrinn',
+];
+let [results, setResults] = useState([...data]);
+const handleInput = (event: Event)=>{
+let query = '';
+
+const target = event.target as HTMLIonSearchbarElement;
+if (target) query = target.value!.toLowerCase();
+setResults(data.filter((d) => d.toLowerCase().indexOf(query) > -1));
+
+};
+
    return(
     <IonPage>
-        <IonHeader>
-            
+        <IonHeader>             
             <IonToolbar>
                 <IonButtons>
-                    <IonMenuButton>
-
-                    </IonMenuButton>
+                    <IonMenuButton></IonMenuButton>
                 </IonButtons>
                 <IonTitle>Search</IonTitle>
             </IonToolbar>
+
+        <IonSearchbar debounce={1000} onIonInput={(event) => handleInput(event)}>
+        </IonSearchbar>
         </IonHeader>
         <IonContent className="ion-padding" >
-<h1>Search</h1>
+{results.map((result) => (
+  <IonItem>{result}</IonItem>
+
+))}
+
         </IonContent>
     </IonPage>
 
